f239765a6321b338fd9ac9088373f6445a6528c3
Marcelo <marcelo_4940@hotmail.com>
2023-01-08 21:38:27 -0300

    Commit inicial

---
+++ src/main/java/com/rabbitmq/tools/message/MessageController.java
@@ -0,0 +1,21 @@
+package com.rabbitmq.tools.message;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/message")
+public class MessageController {
+
+    @Autowired
+    private MessageService messageService;
+
+    @PostMapping("send")
+    public ResponseEntity sendMessage() {
+        messageService.sendMessage();
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/rabbitmq/tools/message/MessageService.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.tools.message;
+
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
+public class MessageService {
+
+    public void sendMessage() {
+        Logger.getLogger("MessageService").warning("sendMessage");
+    }
+}
