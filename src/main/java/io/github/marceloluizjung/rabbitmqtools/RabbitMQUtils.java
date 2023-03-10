4136ba043608e4396c835ea0602c3ec02ab749e2
Marcelo <marcelo_4940@hotmail.com>
2023-03-09 21:19:56 -0300

    vai que vai

--- src/main/java/io/github/marceloluizjung/rabbitmqtools/RabbitMQUtils.java
+++
@@ -1,5 +0,0 @@
-package io.github.marceloluizjung.rabbitmqtools;
-
-public interface RabbitMQUtils {
-    void sendMessage();
-}
--- src/main/java/io/github/marceloluizjung/rabbitmqtools/message/MessageService.java
+++ src/main/java/io/github/marceloluizjung/rabbitmqtools/message/MessageService.java
@@ -1,14 +1,12 @@
 package io.github.marceloluizjung.rabbitmqtools.message;
 
-import io.github.marceloluizjung.rabbitmqtools.RabbitMQUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.logging.Logger;
 
 @Service
-public class MessageService implements RabbitMQUtils {
+public class MessageService {
 
-    @Override
     public void sendMessage() {
         Logger.getLogger("MessageService").warning("sendMessage");
     }
